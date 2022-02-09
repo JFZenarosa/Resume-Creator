2e159b4c0dbc3e360c2d5279caaf12fc7b8e3d33
JFZenarosa <jaireh62@gmail.com>
2022-02-09 16:39:47 +0800

    Created the PDF Creator, saving Full name into PDF

---
+++ Resume Creator/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Resume_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.create_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // create_button
+            // 
+            this.create_button.Location = new System.Drawing.Point(244, 141);
+            this.create_button.Name = "create_button";
+            this.create_button.Size = new System.Drawing.Size(75, 23);
+            this.create_button.TabIndex = 0;
+            this.create_button.Text = "Create File";
+            this.create_button.UseVisualStyleBackColor = true;
+            this.create_button.Click += new System.EventHandler(this.create_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 230);
+            this.Controls.Add(this.create_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button create_button;
+    }
+}
+
---
+++ Resume Creator/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.draw;
+
+namespace Resume_Creator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void create_button_Click(object sender, EventArgs e)
+        {
+            string jsonpath = @"X:\School\Visual Studio\Repository\Resume Creator\Resume Creator\resumeCreator.json";
+
+            string openfile = File.ReadAllText(jsonpath);
+            jsonreader resumeout = JsonConvert.DeserializeObject<jsonreader>(openfile);
+
+            Document pdfcreator = new Document();
+            PdfWriter.GetInstance(pdfcreator, new FileStream(@"X:\School\Visual Studio\Repository\Resume Creator\Resume Creator\ZENAROSA_JAIREH.pdf",FileMode.Create));
+            pdfcreator.Open();
+            Paragraph fullname = new Paragraph(resumeout.Full_Name);
+            pdfcreator.Add(fullname);
+            pdfcreator.Close();
+            
+        }
+
+        public class jsonreader
+        {
+            public string Full_Name
+            {
+                get;set;
+            }
+        }
+    }
+}
